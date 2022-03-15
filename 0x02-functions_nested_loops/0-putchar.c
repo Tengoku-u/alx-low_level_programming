cab9ade0abc6a0f1b7beea62d8926acfaa6a8205
Your Name <you@example.com>
2022-03-15 15:04:31 -0700

    sss

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,15 +9,15 @@
 
 int main(void)
 {
-  char s[9] = "_putchar";
-  int i = 0;
+char s[9] = "_putchar";
+int i = 0;
 
-  while (i < 8)
-    {
-      _putchar(s[i]);
-      i++;
-    }
-  _putchar('\n');
+while (i < 8)
+{
+_putchar(s[i]);
+i++;
+}
+_putchar('\n');
 
 return (0);
 }
