e8332537c52c3077ef71a83501e5d930caf9006f
Your Name <you@example.com>
2022-03-15 12:46:33 -0700

    sss

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line.
+ *
+ * Return: 0 (Success)
+ */
+
+
+int main(void)
+{
+char *_putchar;
+
+for (_putchar = "_putchar\n"; *_putchar != '\0'; ++_putchar)
+_putchar(*_putchar);
+return (0);
+}
