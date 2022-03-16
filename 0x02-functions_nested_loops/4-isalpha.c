fe6859830509374b01590537dbfe34f418f6d7d1
Your Name <you@example.com>
2022-03-15 17:01:50 -0700

    sss

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -17,7 +17,8 @@ for (lower = 'a'; lower <= 'z'; lower++)
 {
 for (upper = 'A'; upper <= 'Z'; upper++)
 {
-if (c == lower || c == upper)														isletter = 1;
+if (c == lower || c == upper)
+isletter = 1;
 }	
 }
 return (isletter);
