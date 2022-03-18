32e7fb0705320b7e26080226a8646ceb327c6282
Your Name <you@example.com>
2022-03-18 14:46:32 -0700

    ddd

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,9 @@
  *Function prototypes
  */
 
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -13,3 +16,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char c);
+
+#endif
