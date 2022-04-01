b53b95606fbc527cc90970966cd75c4b632118cc
Your Name <you@example.com>
2022-04-01 16:55:12 -0700

    static lib add

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+char s[9] = "_putchar";
+int i = 0;
+
+while (i < 8)
+{
+_putchar(s[i]);
+i++;
+}
+_putchar('\n');
+
+return (0);
+}
