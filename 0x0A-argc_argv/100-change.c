79f084dc3674eae51519eaf2e0c14adac68c5c03
Your Name <you@example.com>
2022-04-01 16:08:48 -0700

    coin count

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,46 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
-
-/**
- * coin_count - counts the min coins needed for change
- *
- * @argv: argument vector
- * @count: int to count coins
- * Return: int
- */
-
-int coin_count(char *argv[], int count)
-{
-int sum = 0;
-
-while (!(sum + 25 > atoi(argv[1])))
-{
-sum += 25;
-count++;
-}
-while (!(sum + 10 > atoi(argv[1])))
-{
-sum += 10;
-count++;
-}
-while (!(sum + 5 > atoi(argv[1])))
-{
-sum += 5;
-count++;
-}
-while (!(sum + 2 > atoi(argv[1])))
-{
-sum += 2;
-count++;
-}
-while (!(sum + 1 > atoi(argv[1])))
-{
-sum += 1;
-count++;
-}
-return (count);
-}
+#include "main.h"
 
 /**
  * main - prints the min num of coins to make change for an amount of money
