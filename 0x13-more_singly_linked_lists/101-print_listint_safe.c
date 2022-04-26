20b56e49f7d434b868d5cd0942e7a895b0e3462d
Your Name <you@example.com>
2022-04-25 17:29:56 -0700

    ddd

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,33 +1,13 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints list, avoids loop
- * @head: points to start of list
- * Return: number of nodes in size_t or exit 98
+ * print_listint_safe - define function
+ * @head:
+ * Return: what does it return?
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-
-size_t count = 0;
-
-while (head)
-{
-printf("[%p] %d\n", (void *)head, head->n);
-count += 1;
-
-if (head > head->next)
-{
-head = head->next;
+	head++;
+	return (0);
 }
-else
-{
-head = head->next;
-printf("-> [%p] %d\n", (void *)head, head->n);
-break;
-}
-}
-return (count);
-}
-
-
